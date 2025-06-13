595ede9a91e2c27c16664c971e6da6c3e23edb6c
abhishekbaloria <abhia3477@gmail.com>
2025-06-13 16:38:17 +0200

    Create index.py

---
+++ index.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+import random
+st.set_config(page_title="B-Roll Assistant", layout="wide") 
+
+#title and description 
+st.title("🎬 B-Roll Assistant")
+st.markdown("Generate high-quality b-rolls  based on your video type.")
